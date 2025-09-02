2f64e957bcf62c2b44f6dd24331e9c7c310975a8
Ehsan Mohammadi <ehsanx64@gmail.com>
2025-09-02 15:18:31 +0330

    refactor: updates to accomodate chakra ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Button } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
@@ -10,9 +9,9 @@ function App() {
     <>
       <h1>FiberSpace</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
       </div>
     </>
   )
